a5b30108611d62f34b00307c52401f0712af9e52
Kevin Channon <kevin.channon@paconsulting.com>
2021-11-10 13:06:41 +0000

    Adding forward find

--- staticmap/static_map.hpp
+++ staticmap/static_map.hpp
@@ -3,6 +3,8 @@
 #include <array>
 #include <utility>
 #include <algorithm>
+#include <stdexcept>
+#include <format>
 
 template<typename Key_T, typename Value_T, size_t ITEM_COUNT>
 class StaticMap
@@ -18,6 +20,16 @@ public:
 	}
 
 	constexpr size_type size() const { return m_items.size(); }
+
+	const Value_T& at(const Key_T& k) const
+	{
+		const auto it = std::lower_bound(m_items.cbegin(), m_items.cend(), k, [](const auto& x1, const Key_T& x2) { return x1.first <= x2; });
+		if (m_items.cend() == it) {
+			throw std::out_of_range("Invalid item");
+		}
+
+		return it->second;
+	}
 
 private:
 	std::array<value_type, ITEM_COUNT> m_items;
--- staticmap/staticmap.cpp
+++ staticmap/staticmap.cpp
@@ -23,5 +23,17 @@ namespace staticmap
 			const auto m = StaticMap<int, std::string, 5>{ {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"}, {5, "Five"} };
 			Assert::AreEqual(size_t{ 5 }, m.size());
 		}
+
+		TEST_METHOD(ForwardAt)
+		{
+			const auto m = StaticMap<int, std::string, 5>{ {1, "One"}, {2, "Two"}, {3, "Three"}, {4, "Four"}, {5, "Five"} };
+			
+			const auto expected = std::array<std::string, 5>{ "One", "Two", "Three", "Four", "Five" };
+
+			for ( int i = 0; i < 5; ++i )
+			{
+				Assert::AreEqual(expected[i], m.at(i));
+			}
+		}
 	};
 }
