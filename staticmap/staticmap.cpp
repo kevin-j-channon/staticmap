e8a35764eac68c56eec1897da8d7104115313383
Kevin Channon <kevin.channon@paconsulting.com>
2021-11-10 12:16:48 +0000

    Add project files.

---
+++ staticmap/staticmap.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace staticmap
+{
+	TEST_CLASS(staticmap)
+	{
+	public:
+		
+		TEST_METHOD(Create)
+		{
+		}
+	};
+}
